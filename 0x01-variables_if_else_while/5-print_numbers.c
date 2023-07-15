4338b81e16a83d70682963e48ce0b25d33154474
humade <hayfordkojo18@gmail.com>
2023-07-14 21:49:49 -0700

    Number 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Runs from start to finish
+ *
+ * Description: Prints single digit no's
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
