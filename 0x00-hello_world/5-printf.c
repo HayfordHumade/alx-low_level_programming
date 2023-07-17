ffc6c2335cce0573fdfe77221d3847669748c0f3
humade <hayfordkojo18@gmail.com>
2023-07-16 17:20:25 -0700

    Change printf file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * Main function - entry point
- * Return - 0 success
+ * main - entry point and return point
+ *
+ * Description: This uses printf to print text
+ * Return: 0 always
  */
 int main(void)
-/*Main function- entry point*/
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
