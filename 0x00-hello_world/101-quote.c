bbe1c505b186a4160d0e5660e3ddf0a6cfa8c9b0
humade <hayfordkojo18@gmail.com>
2023-07-17 18:04:35 -0700

    Edit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Run always
+ *
+ * Descrition: Prints line of text
+ * Return: Always zero
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+	return (0);
+}
