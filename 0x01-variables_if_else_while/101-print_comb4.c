8ad21d9f4e70fa9a891c8c9c568993d6bebc92e1
humade <hayfordkojo18@gmail.com>
2023-07-17 19:03:19 -0700

    Change

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 /**
- * main - runs over
+ * main - Runs from start to finish
  *
- * Description: HERE
- * Return: zero
+ * Description: Combinations of digits
+ * Return: returns 0
  */
 int main(void)
 {
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 8; x++)
+	{
+		for (y = x + 1; y <= 8; y++)
+		{
+			for (z = y + 1; z <= 9; z++)
+			{
+				if (x == y || y == z || x == z)
+				{
+					continue;
+				}
+				putchar(48 + x);
+				putchar(48 + y);
+				putchar(48 + z);
+				if (x == 7)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
