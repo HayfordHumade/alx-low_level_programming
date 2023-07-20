ec8cd3e6cf5f6fa63466c86c9f33bd9e53b41f5c
humade <hayfordkojo18@gmail.com>
2023-07-19 18:22:40 -0700

    Add a readme.md file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - It all starts and end here
+ *
+ * Description: Using fuction puts to print
+ * Return: always output 0
+ */
+int main(void)
+{
+	return (0);
+}
