a6a418965188b9686db288092d10d7a08c1eb1a2
humade <hayfordkojo18@gmail.com>
2023-07-24 19:21:16 -0700

    Compile file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 /**
- * main - Run it all
+ * reset_to_98 - takes pointer to an int
+ * @n: this is an integer pointer variable
  *
- * Description: Something happens in here
- * Return: Always 0.
+ * Description: takes pointer to 98
+ * Return: void
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
+/**
+ * main - Run it all
+ *
+ * Description: Something happens in here
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
-	
+
 	n = 402;
 	printf("n=%d\n", n);
 	reset_to_98(&n);
