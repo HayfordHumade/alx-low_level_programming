020c05206d96f1de9c4caa370f29c73fcb590326
humade <hayfordkojo18@gmail.com>
2023-07-25 20:07:08 -0700

    Print backwards

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,21 @@ void print_rev(char *s)
 {
 	int counter;
 
-	counter = '\0';
-	while (counter != *s)
+	counter = 0;
+	if (*(s + counter) == 0)
 	{
-		_putchar(*(s + counter));
-		counter--;
+		int i;
+		int reduce;
+
+		reduce = counter - 1;
+		for (i = reduce; i <= 0; i++)
+		{
+			_putchar(*(s + reduce));
+		}
+	}
+	else
+	{
+		counter++;
 	}
 	_putchar('\n');
 }
