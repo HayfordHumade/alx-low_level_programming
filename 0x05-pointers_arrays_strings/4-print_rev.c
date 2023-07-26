076a7962e30ddfd185f5d692196401dea0095183
humade <hayfordkojo18@gmail.com>
2023-07-25 20:21:57 -0700

    Change file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,20 +12,20 @@ void print_rev(char *s)
 	int counter;
 
 	counter = 0;
-	if (*(s + counter) == 0)
-	{
-		int i;
-		int reduce;
-
-		reduce = counter - 1;
-		for (i = reduce; i <= 0; i++)
-		{
-			_putchar(*(s + reduce));
-		}
-	}
-	else
+	while (*(s + counter) != 0)
 	{
 		counter++;
+		continue;
+	}
+
+	int i;
+	int reduce;
+
+	reduce = counter - 1;
+	for (i = reduce; i <= 0; i++)
+	{
+		_putchar(*(s + reduce));
+		reduce--;
 	}
 	_putchar('\n');
 }
