bd0c09056b69bf33e501496cba86b4d0a6376277
humade <hayfordkojo18@gmail.com>
2023-08-07 17:57:53 +0000

    Check case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - Checks for lower case characters
+ * @c: Variable that accepts integer values
+ *
+ * Description: Uses c to receive variable sto process
+ * Return: 1 if c is lowercase or 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 || c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
