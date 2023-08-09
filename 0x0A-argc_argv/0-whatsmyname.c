9dd5a30e06057f590e96ae9d33d208de27e67e3a
humade <hayfordkojo18@gmail.com>
2023-08-09 21:42:52 +0000

    Create file for argc & v

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - Runtime starts and ends here
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Description: This program prints its name
+ * Return: name.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
