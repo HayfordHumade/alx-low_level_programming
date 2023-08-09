3033edebe14ba03b8b74771537c542e362a95146
humade <hayfordkojo18@gmail.com>
2023-08-09 21:51:43 +0000

    Output size of argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Runs start to finish
+ * @argc: Argument count
+ * @argv: A one dimensional array of character argument
+ *
+ * Description: Prints the size of argument vector, argv
+ * Return: Size integer value
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d/n", argc);
+}
