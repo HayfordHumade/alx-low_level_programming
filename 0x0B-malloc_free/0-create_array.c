4cac06dedcb04d68804630c66145ad54704c3d5f
humade <hayfordkojo18@gmail.com>
2023-08-10 05:48:48 +0000

    Edit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int counter;
 	char *array;
 
-	arr = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 
