de024de38d75260881aa3c699392ff3ceb194afd
humade <hayfordkojo18@gmail.com>
2023-08-11 03:03:36 +0000

    Void *pointer

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,7 +10,7 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *memory;
+	void *memory;
 
 	memory = malloc(b);
 	if (memory == NULL)
@@ -18,5 +18,4 @@ void *malloc_checked(unsigned int b)
 		_putchar('98');
 		return (1);
 	}
-	free(memory);
 }
