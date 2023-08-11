aeb979b01f67ce8efd386a46c115eb0ef5906f30
humade <hayfordkojo18@gmail.com>
2023-08-11 02:40:27 +0000

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - Function allocates memory using malloc
+ * @b: Number of integer values
+ *
+ * Description: Checks malloc for success or fail too
+ * Return: 98 for malloc fail
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *memory;
+
+	memory = malloc(b);
+	if (memory == NULL)
+	{
+		printf("%d", 98);
+		return (0);
+	}
+
+}
