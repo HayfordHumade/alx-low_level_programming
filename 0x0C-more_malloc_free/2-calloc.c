5f5d49ae4c7f2259821c0b9e97a83c849ff927c9
humade <hayfordkojo18@gmail.com>
2023-08-12 23:01:49 +0000

    Function creates array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,5 +11,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	void *array[];
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(sizeof(size) * nmemb);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		array[i] = 0;
+	}
+
+	return (array);
 }
