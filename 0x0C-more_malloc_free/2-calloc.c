d2ec123895d5b46714a784661cbe1c3d3164957c
humade <hayfordkojo18@gmail.com>
2023-08-12 22:09:35 +0000

    Edit 2*

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,28 +6,10 @@
  * @nmemb: integer
  * @size: size integer
  *
- * Description: This does that
+ * Description: This function allocates memory for an array
  * Return: NULL
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int x = 0;
-	int y = 0;
-	char *p;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	y = nmemb * size;
-	p = malloc(l);
-
-	if (p == NULL)
-		return (NULL);
-
-	while (x < y)
-	{
-		p[x] = 0;
-		x++;
-	}
-	return (p);
+	
 }
