810ad0f9776e06f8ab375b5f863936412ffd9693
humade <hayfordkojo18@gmail.com>
2023-08-12 16:05:49 +0000

    Edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,11 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	char *string;
 	unsigned int i;
 	unsigned int j;
@@ -20,10 +25,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s2_concat;
 	unsigned int total_length;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	while (s1[length_s1] != '\0')
 	{
 		length_s1++;
