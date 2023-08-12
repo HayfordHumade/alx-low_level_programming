b7e050321d74757ede91db705919eedd114fdddc
humade <hayfordkojo18@gmail.com>
2023-08-12 16:34:09 +0000

    Add -1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < length_s2; j++)
 		string[length_s1 + j] = s2[j];
 
-	string[total_length] = '\0';
+	string[total_length - 1] = '\0';
 
 	return (string);
 }
