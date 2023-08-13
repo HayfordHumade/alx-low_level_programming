9d0fe7828a2832783f16073b270796ca572c8be9
humade <hayfordkojo18@gmail.com>
2023-08-13 00:45:13 +0000

    Edit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,15 +13,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(size * nmemb);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < (size * nmemb); i++)
+	while (i < (nmemb * size))
+	{
 		array[i] = 0;
+		i++
+	}
 
 	return (array);
 }
