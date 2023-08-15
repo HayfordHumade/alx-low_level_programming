e07c3378316ba9d82600671d8e30558a2ec79f16
humade <hayfordkojo18@gmail.com>
2023-08-15 05:05:14 +0000

    Add semi colon in dog.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * *new_dog - Create new dog with dog_t
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Description: This funcitons creates a new dog with details
+ * Return: nothing.
+ */
+dog_t *newdog(char *name, float *age, char *owner)
+{
+	if (NULL)
+		return (NULL);
 
+}
