dac28a89c5b3c7f0330e7ee2a5756d82186ae2bb
humade <hayfordkojo18@gmail.com>
2023-08-16 01:28:58 +0000

    Hell yeah

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,37 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
+ * array_char - Function to allocate memory
+ * @string: Takes char pointers
+ *
+ * Description: This function allocates the right memory and returns
+ * a pointer to the memory
+ * Return: Pointer to char allocated memory.
+ */
+void array_char(char *string)
+{
+	int string_length, i, memory_size;
+	char *ptr;
+
+	string_length = 0;
+	while (string[string_length] != '\0')
+		string_length++;
+	memory_size = string_length + 1;
+	/* Allocates memory/bytes of 'char' * 'number of characters' */
+	ptr = malloc(memory_size);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < string_length)
+	{
+		ptr[i] = string[i];
+		i++;
+	}
+	ptr[string_length] = '\0';
+
+	return (ptr);
+}
+/**
  * new_dog - Create new dog with dog_t
  * @name: Name
  * @age: Age
@@ -13,13 +44,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	char *dog_name;
+	char *dog_owner;
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
-	newdog->age = age;
-	newdog->owner = owner;
+	dog_name = array_char(name);
+	dog_owner = array_char(owner);
 
-	return (newdog);
+	free(name);
+	free(owner);
+
+	newdog->name = dog_name;
+	newdog->age = age;
+	newdog->owner = dog_owner;
 }
