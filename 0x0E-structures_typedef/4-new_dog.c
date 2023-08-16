3e100de2d9176249b9a7ffbd0cb61c7173347fc3
humade <hayfordkojo18@gmail.com>
2023-08-16 00:24:33 +0000

    Function newdog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,26 +2,24 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * _strcopy - Returns 
- * @src: copy string
- * @dest: copy string here
- *
- * Return: dest
- */
-char *_strcopy(char *dest, char *src)
-{
-	int x;
-
-	for (x = 
-/**
- * *new_dog - Create new dog with dog_t
+ * *newdog - Create new dog with dog_t
  * @name: Name
  * @age: Age
  * @owner: Owner
  *
- * Description: This funcitons creates a new dog with details
+ * Description: Funciton creates a new dog structure using the typedef 'dog_t'
  * Return: nothing.
  */
-dog_t *newdog(char *name, float *age, char *owner)
+dog_t *newdog(char *name, float age, char *owner)
 {
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
 }
