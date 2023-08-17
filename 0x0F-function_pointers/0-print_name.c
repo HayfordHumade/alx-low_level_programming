157c6068528315939c80afa7745aefe3922df789
humade <hayfordkojo18@gmail.com>
2023-08-17 03:23:48 +0000

    Call back function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - A fucntion that prints a name
+ * @name: Name of person
+ * @f: A function pointer
+ *
+ * Description: This function takes two parameters, a char and funciton
+ * pointer.
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
