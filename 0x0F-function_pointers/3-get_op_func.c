d3f3cb48546645eb497e3ba7aec283e4e58b9afc
humade <hayfordkojo18@gmail.com>
2023-08-17 17:00:32 +0000

    Code 3-get* function ste step 1:

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * (*get_op_func(char *s))(int, int) - This function returns a function pointer
+ * @s: Receives operator +, -, /, % or *
+ *
+ * Description: Checks which function operator 's' corresponds to
+ * Return: function pointer.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+}
