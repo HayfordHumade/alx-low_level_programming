b9b7acf4373c57b97ec6d8bdcc79886647791006
humade <hayfordkojo18@gmail.com>
2023-08-18 02:29:54 +0000

    Edit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,34 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
-		int num = va_arg(numbers, int);
-		int tmp = num;
-		if (tmp < 0)
-		{
-			_putchar('-');
-			num = -num;
-		}
-		
-		int num_digits = 1;
-		
-		while (tmp /= 10)
-			num_digits *= 10;
-		while (num_digits)
-		{
-			int digit = num / num_digits;
-
-			_putchar(digit + '0');
-			num %= num_digits;
-			num_digits /= 10;
-		}
+		printf("%d", va_arg(numbers, int));
 		if (i != (n - 1) && separator != NULL)
-		{
-			while (*separator)
-			{
-				_putchar(*separator);
-				separator++;
-			}
-		}
-		_putchar('\n');
-		var_end(numbers);
+			printf("%s", separator);
+	}
+	printf("\n");
+	var_end(numbers);
 }
