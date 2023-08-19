50dda3b5f6f6ec2ec0b41537e916fdb4c056dfbd
humade <hayfordkojo18@gmail.com>
2023-08-19 02:29:02 +0000

    Use -> to access *'s

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,14 +15,16 @@ int (*get_op_func(char *s))(int, int)
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
-		{"%", op_mod}
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (i < 6)
 	{
-		if (ops[i].op == s)
-			return (ops[i].f);
+		if (ops[i]->op == s)
+			return (ops[i]->f);
+		i++;
 	}
 	return (NULL);
 }
