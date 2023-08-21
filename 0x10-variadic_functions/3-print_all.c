9d36aa222e966ccc976cba7e2bf3df4e078b18ee
humade <hayfordkojo18@gmail.com>
2023-08-21 23:24:09 +0000

    Remove

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_all - Prints everything
+ * print_all - Function prints everything
  * @format: Contains argument types
  *
  * Description: Prints everything?
@@ -11,44 +11,4 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str;
-	char *se;
-
-	se = "";
-	va_list list;
-
-	va_start(list, format);
-	if (format)
-	{
-		while (format[i])
-		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", se, va_arg(list, int));
-					break;
-				case 'i':
-					printf("%s%d", se, va_arg(list, int));
-					break;
-				case 'f':
-					printf("%s%f", se, va_arg(list, double));
-					break
-				case 's':
-					str = va_arg(list, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", se, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			se = ",";
-			i++;
-		}
-	}
-	printf("\n");
-	va_end(list);
 }
-
