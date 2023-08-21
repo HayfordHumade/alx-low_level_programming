ca1af902f8ff8a04383b71e639b9462c54bfe862
humade <hayfordkojo18@gmail.com>
2023-08-21 23:42:57 +0000

    My personal files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "variadic_functions.h"
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
- * sum_them_all - Function to sum them all
- * @n: Number of arguments passed into the function
- * @...: Elipsis represents a vriable number of arguments
+ * sum_them_all - Function returns the sum of all its parameters
+ * @n: Constant n which tells the number of parameters?
+ * ...: Ellipsis to show it takes a series of arguements
  *
- * Description: This function adds all parameters
- * Return: Sum of arguments. if n==0, 0
+ * Description: Uses a variadic function template to do this
+ * Return: Returns sum.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list app;
-	unsigned int x;
-	unsigned int sum = 0;
+	va_list ap;
+	unsigned int i;
+	int sum;
 
-	va_start(app, n);
-	for (x = 0; x < n; x++)
+	va_start(ap, n);
+	i = 0;
+	sum = 0;
+	while (i < n)
 	{
-		sum = sum + va_arg(app, int);
+		int temp = va_arg(ap, int);
+
+		sum += temp;
+		i++;
 	}
-	va_end(app);
+	va_end(ap);
 
 	return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,31 +3,42 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_strings - Function print string and then a new line
- * @separator: Between strings
- * @n: Number of strings
- * @...: Variable number of strings
+ * print_strings - Function prints strings followed by a new line
+ * @separator: String to be printed between the strings
+ * @n: Number of strings passed to the function
+ * ...: Ellipsis indicating variadic function taking other parameters
  *
- * Description: This function prints strings with seprators
- * Return: Strings?
+ * Description: Prints strings with separators between them
+ * Return: Strings.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list strs;
-	char *str;
+	va_list ap;
 	unsigned int i;
 
-	va_start(strs, n);
-	for (i = 0; i < n; i++)
+	va_start(ap, n);
+	i = 0;
+	if (separator == NULL)
 	{
-		str = va_arg(strs, char *);
-		if (str == NULL)
+		va_end(ap);
+		return;
+	}
+	while (i < n)
+	{
+		char *string = (char *)va_arg(ap, char *);
+
+		if (string == NULL)
+		{
 			printf("(nil)");
+		}
 		else
-			printf("%s", str);
-		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", string);
+		}
+		if (i < (n - 1))
 			printf("%s", separator);
+		i++;
 	}
 	printf("\n");
-	va_end(strs);
+	va_end(ap);
 }
