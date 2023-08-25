fd222c52dd85a9d7f6f58a375f4f3923cb8b4ce6
humade <hayfordkojo18@gmail.com>
2023-08-25 15:57:07 +0000

    Fix: change len type to %lu

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *n_node;
-	int length;
+	unsigned int length;
 
 	n_node = (list_t *)malloc(sizeof(list_t));
 	n_node->str = strdup(str);
