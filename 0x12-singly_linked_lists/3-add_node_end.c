f06c9bb8a8a380b9332abc8e21ba5c2339e6a7c3
humade <hayfordkojo18@gmail.com>
2023-08-25 19:55:02 +0000

    Clean: stick to 40 lines

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,23 +17,19 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	/* allocate memory for new node */
 	n_node = (list_t *)malloc(sizeof(list_t));
-
 	/* check if new node is NULL before using it */
 	if (n_node != NULL)
 	{
 		/* copy str into new node str */
 		n_node->str = strdup(str);
-
 		/* evaluate for the lenght of str if str isn't null */
 		if (n_node->str != NULL)
 		{
 			while (n_node->str[len] != '\0')
 				len++;
 		}
-
 		/* assign length of str to ->len */
 		n_node->len = len;
-
 		/* check *head before using it */
 		if (*head == NULL)
 		{
@@ -41,17 +37,13 @@ list_t *add_node_end(list_t **head, const char *str)
 			n_node->next = NULL;
 			return (n_node);
 		}
-
 		/* assign current to head */
 		current = *head;
-
 		/* find the end of linked list */
 		while (current->next != NULL)
 			current = current->next;
-
 		/* add new node by replacing NULL */
 		current->next = n_node;
-
 		/* create last node to be NULL */
 		current->next->next = NULL;
 	}
