1a6bb933ec63f6945b4c5faccbc64d3ac062d9d8
humade <hayfordkojo18@gmail.com>
2023-08-26 00:22:13 +0000

    Fix: free list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_list - frees list_t
+ * @head: head of a linked list
+ *
+ * Description: frees an entire linked list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		/* set temp to head */
+		temp = head;
+
+		/* set head to next list_t */
+		head = head->next;
+
+		/* free temp */
+		free(temp);
+	}
+}
