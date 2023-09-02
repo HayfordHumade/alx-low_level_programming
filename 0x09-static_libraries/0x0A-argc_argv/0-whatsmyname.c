6a99d6b1645ab31944fa90c60900c66de805113a
humade <hayfordkojo18@gmail.com>
2023-09-02 02:28:49 +0000

    Add: task 0

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - runs it all
+ * @argc: number of arguments
+ * @argv: array of pointers to string
+ *
+ * Description: prints the name of the program
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
