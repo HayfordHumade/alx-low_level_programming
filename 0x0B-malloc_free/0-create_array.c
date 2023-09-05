365112a8227772345eb3a1991d67ebad1c03d2b6
humade <hayfordkojo18@gmail.com>
2023-09-05 17:22:36 +0000

    Fix: check char_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	char_array = malloc(size);
-	if (size == NULL)
+	if (char_array == NULL)
 		return (NULL);
 	while (i < size)
 	{
