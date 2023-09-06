8869b10a18cbf66ae64267bd72c76be78dda79e3
humade <hayfordkojo18@gmail.com>
2023-09-06 01:40:06 +0000

    Edit: check for empty str

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,12 @@ char *_strdup(char *str)
 	int i, size;
 
 	size = 0;
-	/* check str before use */
-	if (str == NULL)
-		return (NULL);
 	/* determine size of str */
 	while (str[size] != '\0')
 		size++;
+	/* check str before use */
+	if (str == NULL || size < 1)
+		return (NULL);
 	/* allocate memory for new str */
 	new_str = malloc(sizeof(char) * (size + 1));
 	/* check allocated memory before use */
