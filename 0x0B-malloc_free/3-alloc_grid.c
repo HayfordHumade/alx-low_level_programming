6d33f2b97ff59b2623e0c882f123a15a823b3b1a
humade <hayfordkojo18@gmail.com>
2023-09-07 02:57:25 +0000

    Fix: free arr before return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,10 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *)malloc(sizeof(int) * width);
 	/* check arr for NULL */
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 	/* initializing 2D array */
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
