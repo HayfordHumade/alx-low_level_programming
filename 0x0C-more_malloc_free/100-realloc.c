4c2ce693d66e0106a821768dc08385d2001cc4f6
humade <hayfordkojo18@gmail.com>
2023-09-08 02:58:33 +0000

    Add: mul

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_realloc - Allocates a memory block and free
+ * @ptr: Pointer to old memory block
+ * @old_size: Unsigned integer giving old memory size
+ * @new_size: Unsigned integer giving new memory size
+ *
+ * Description: This function increases the size of an already
+ * allocated memory.
+ * Return: pointer ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	/* Allocate memory of new_size */
+	if (new_size > old_size)
+		new_ptr = malloc(new_size);
+	/* Free memory block, ptr */
+	free(ptr);
+
+	return (new_ptr);
+}
