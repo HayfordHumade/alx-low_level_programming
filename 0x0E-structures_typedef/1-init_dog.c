7d41ede2d42b0d90ad042a7061885c0ebc3f6964
humade <hayfordkojo18@gmail.com>
2023-09-11 20:39:15 +0000

    Edit: check struct only

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,10 +16,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return (NULL);
-	if (name == NULL)
-		name = "Unknown";
-	if (owner == NULL)
-		owner = "Unknown";
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
