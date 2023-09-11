3b484b6177cd799fc0aaa94725adcf72bdc2a739
humade <hayfordkojo18@gmail.com>
2023-09-11 21:13:20 +0000

    Edit: use if else to check d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,14 +14,17 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		exit(98);
 	if (name == NULL)
 		name = "";
 	if (owner == NULL)
 		owner = "";
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+		exit(98);
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
