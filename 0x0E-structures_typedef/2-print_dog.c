57775d2c9064646a15b07a5468eeb171bd75eb91
humade <hayfordkojo18@gmail.com>
2023-09-11 21:27:22 +0000

    Add: prints struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include "dog.h"
+/**
+ * print_dog - prints struct dog
+ * @d: dog parameter
+ *
+ * Description: Using a string format specifier, print
+ * information about a dog.
+ * Return: nothing.
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner = NULL)
+		d->owner = "(nil)";
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s",
+				d->name, d->age, d->owner);
+	}
+}
