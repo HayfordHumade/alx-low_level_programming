73a2c52d37510304134fce13985b5b01376f7571
humade <hayfordkojo18@gmail.com>
2023-09-12 00:24:50 +0000

    Edit: check pointers

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,11 +19,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_name = name;
 	new_owner = owner;
+	if (new_name == NULL)
+		new_name = "";
+	if (new_owner == NULL)
+		new_owner = "";
 	/* allocate memory for new_dog */
 	new_dog = malloc(sizeof(dog_t));
 	/* always check if memory allocation is a success */
 	if (new_dog != NULL)
 	{
+		if (new_name == NULL)
+			new_owner = "";
+		if (new_name == NULL)
+			new_owner == "";
 		new_dog->name = new_name;
 		new_dog->age = age;
 		new_dog->owner = new_owner;
