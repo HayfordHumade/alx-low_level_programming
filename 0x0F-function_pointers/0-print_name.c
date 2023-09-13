23808f127b9fea208836e579611fc35f2f7897be
humade <hayfordkojo18@gmail.com>
2023-09-13 18:01:57 +0000

    Add: print_name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: to be printed
+ * @f: a function pointer
+ *
+ * Description: prints a name
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	/* pass name into the function pointer f */
+	f(name);
+}
