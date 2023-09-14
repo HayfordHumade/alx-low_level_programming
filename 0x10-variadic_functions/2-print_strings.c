04a14912146ed15a660eb221a947168c59a6fa92
humade <hayfordkojo18@gmail.com>
2023-09-14 21:50:43 +0000

    Edit: indent code properly

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		/* check position of string & separator for NULL */
 		if (i < (n - 1) && separator != NULL)
-				printf("%s", separator);
+			printf("%s", separator);
 	}
 	/* new line */
 	printf("\n");
