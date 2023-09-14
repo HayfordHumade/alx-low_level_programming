af7bec6230a9f71638ce8b45e41229a78a2b7490
humade <hayfordkojo18@gmail.com>
2023-09-14 00:43:20 +0000

    Fix: find index before passing

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -25,10 +25,8 @@ int (*get_op_func(char *s))(int, int)
 	{
 		i = 0;
 		while (ops[i].f != NULL || *ops[i].op != *s)
-		{
-			return (ops[i].f);
 			i++;
-		}
+		return (ops[i].f);
 	}
 
 	return (NULL);
