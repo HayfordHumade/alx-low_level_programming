6494e82043544451528d3e1e79cc7957395f56f3
humade <hayfordkojo18@gmail.com>
2023-09-14 17:48:56 +0000

    Add: sum_them_all function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum all arguments
+ * @n: number of integers
+ * @...: ellipsis to receive an indefinite number of arguments
+ *
+ * Description: function receives a first argument to be the number of arguments
+ * and adds them one after the other and returns the result.
+ * Return: sum.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	/* declare a list */
+	va_list ap;
+	unsigned int i;
+	int sum;
+
+	if (n <= 0)
+		return (0);
+	sum = 0;
+	/* initialize the list */
+	va_start(ap, n);
+	/* loop through the list & add the values to sum */
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	/* clean ap */
+	va_end(ap);
+
+	return (sum);
+}
