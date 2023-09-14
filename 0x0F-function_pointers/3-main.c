2d497682257beb1221396f9ef6b5068149cf0311
humade <hayfordkojo18@gmail.com>
2023-09-14 02:11:13 +0000

    Edit: check argument count first

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,30 +11,36 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+	int num1, num2, result;
+	char op, *p;
 	int (*function)(int, int);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	op = argv[2];
+	/* check number of arguments */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (op == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	if ((*op == '\\' || *op == '%') && (num2 == 0))
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	printf("%d\n", num2);
+	op = *argv[2];
+	p = &op;
+	/* check for division by or modulo 0 */
+	if ((op == '\\' || op == '%') && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	function = get_op_func(op);
-	printf("%d\n", function(num1, num2));
+	function = get_op_func(p);
+	/* check function for NULL */
+	if (function == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = function(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
