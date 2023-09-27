0fec15c78daadf77bb1fe58b795fe0c25cde6c9f
humade <hayfordkojo18@gmail.com>
2023-09-27 22:31:31 +0000

    Fix segmentation fault

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,11 @@ void free_listint2(listint_t **head)
 	listint_t *new_head, *temp;
 
 	new_head = *head;
-	free(head);
 	while (new_head != NULL)
 	{
 		temp = new_head;
 		new_head = new_head->next;
 		free(temp);
 	}
+	free(new_head);
 }
