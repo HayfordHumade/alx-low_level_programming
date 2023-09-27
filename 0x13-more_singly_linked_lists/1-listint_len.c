7f941179cba635da97f098e5e454f173c36b8b8d
humade <hayfordkojo18@gmail.com>
2023-09-27 10:20:32 +0000

    Fix error

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,7 +11,7 @@
 size_t listint_len(const listint_t *h)
 {
 	/* declare a list_t list */
-	const list_t *temp;
+	const listint_t *temp;
 	/* declare variable to hold number of nodes */
 	size_t num;
 
