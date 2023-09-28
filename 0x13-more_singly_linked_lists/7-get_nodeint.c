9d971cb299ff6d9ff433c90428869826fc9a4316
humade <hayfordkojo18@gmail.com>
2023-09-28 01:16:13 +0000

    Check temp for NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
 	temp = head;
 	/* locate nth node */
 	for (i = 0; i < index; i++)
+	{
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
 	/* return pointer to node */
 	return (temp);
 }
