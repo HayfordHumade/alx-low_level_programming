52b5b2561dd8d078750b6f3d1e8609b23d0d861e
humade <hayfordkojo18@gmail.com>
2024-02-20 18:36:07 +0000

    Modify code for clarity

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,24 +19,32 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 	int text_length;
 
+	/* check filename for NULL */
 	if (filename == NULL)
 		return (-1);
+	/* get a file descriptor for open with Read & Write for Owner only */
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	/* check file descriptor for fail */
 	if (fd == -1)
 		return (-1);
-	text_length = 0;
-	while (text_content[text_length] != '\0')
-		text_length++;
-	if (text_length >= 1)
+	/* find the length of text_content */
+	if (text_content != NULL)
 	{
+		text_length = 0;
+		while (text_content[text_length] != '\0')
+			text_length++;
+		/* write into the file and return success, 1, or failure, -1 */
 		bytes_written = write(fd, text_content, text_length);
+		/* check write for fail */
 		if (bytes_written == -1)
 		{
 			close(fd);
 			return (-1);
 		}
 	}
+	/* close file, literally */
 	close(fd);
 
+	/* return 1, for success */
 	return (1);
 }
